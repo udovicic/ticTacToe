c2c2dc9c5fda984925d7cd69ef134091aec34740
Stjepan Udovičić <stjepan@udovicic.org>
2011-06-08 01:32:23 +0200

    main.h was missing from code -.-

---
+++ main.h
@@ -0,0 +1,30 @@
+/*
+ *	main.h
+ * 
+ *	Copyright 2011 Stjepan Udovičić <udovicic.stjepan@gmail.com>
+ *	      
+ *	This file is part of TicTacToe.
+ *
+ *	TicTacToe is free software: you can redistribute it and/or modify
+ *	it under the terms of the GNU General Public License as published by
+ *	the Free Software Foundation, either version 3 of the License, or
+ *	(at your option) any later version.
+ *
+ *	TicTacToe is distributed in the hope that it will be useful,
+ *	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *	GNU General Public License for more details.
+ *
+ *	You should have received a copy of the GNU General Public License
+ *	along with TicTacToe.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+int	player, /* current player */
+	moves, /* count moves */
+	field[9]; /* battlefield */
+void go_away( void );
+
+#endif
